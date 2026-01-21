67f77f1e1a5755de6dccaadea776a8687a20f767
22f1001966 <22f1001966@ds.study.iitm.ac.in>
2026-01-21 20:40:45 +0530

    commit changes to main.py file

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+#create n numpy array
+my_array = np.array([1, 2, 3, 4, 5])
+
+#calculate the sum of all elements in the array
+array_sum = np.sum(my_array)
+
+print("The original array: \n ", my_array)
+print("The sum of all elements in the array: \n ", array_sum)
